304634c6536ff54ae180b9953736d913f68b6761
Julia Löschel <juloeschel@gmail.com>
2022-05-21 17:35:11 +0200

    set and style Interests

--- pages/settings.js
+++ pages/settings.js
@@ -1,11 +1,17 @@
 import Navigation from "../public/Components/Navigation";
 import { useState } from "react";
 import styles from "../styles/Settings.module.css";
+import { useEffect } from "react";
 
 export default function Settings() {
   const [radius, setRadius] = useState(0);
-  const [interestedIn, setInterestedIn] = useState([]);
-  //onLcik on Li item toggle das Interesse
+  const [interestedIn, setInterestedIn] = useState({
+    Bar: false,
+    Restaurant: false,
+    Cafe: false,
+    Hotel: false,
+  });
+  const [filter, setFilter] = useState([]);
 
   let locations = [
     { id: 12, text: "test" },
@@ -15,8 +21,6 @@ export default function Settings() {
     const allLocationsRaw = localStorage.getItem("locations");
     locations = JSON.parse(allLocationsRaw);
   }
-
-  console.log(locations);
 
   return (
     <>
@@ -32,11 +36,65 @@ export default function Settings() {
       </div>
       <div className={styles.settings}>
         <h2>what are you looking for?</h2>
-        <ul>
-          <li>Bar</li>
-          <li>Restaurant</li>
-          <li>Cafe</li>
-          <li>Hotel</li>
+        <ul className={styles.interestList}>
+          <li
+            className={interestedIn.Bar ? styles.active : styles.interests}
+            onClick={() => {
+              setInterestedIn(
+                interestedIn,
+                (interestedIn.Bar = !interestedIn.Bar)
+              );
+              setFilter(
+                Object.keys(interestedIn).filter((key) => interestedIn[key])
+              );
+            }}
+          >
+            Bar
+          </li>
+          <li
+            className={
+              interestedIn.Restaurant ? styles.active : styles.interests
+            }
+            onClick={() => {
+              setInterestedIn(
+                interestedIn,
+                (interestedIn.Restaurant = !interestedIn.Restaurant)
+              );
+              setFilter(
+                Object.keys(interestedIn).filter((key) => interestedIn[key])
+              );
+            }}
+          >
+            Restaurant
+          </li>
+          <li
+            className={interestedIn.Cafe ? styles.active : styles.interests}
+            onClick={() => {
+              setInterestedIn(
+                interestedIn,
+                (interestedIn.Cafe = !interestedIn.Cafe)
+              );
+              setFilter(
+                Object.keys(interestedIn).filter((key) => interestedIn[key])
+              );
+            }}
+          >
+            Cafe
+          </li>
+          <li
+            className={interestedIn.Hotel ? styles.active : styles.interests}
+            onClick={() => {
+              setInterestedIn(
+                interestedIn,
+                (interestedIn.Hotel = !interestedIn.Hotel)
+              );
+              setFilter(
+                Object.keys(interestedIn).filter((key) => interestedIn[key])
+              );
+            }}
+          >
+            Hotel
+          </li>
         </ul>
       </div>
       <div className={styles.settings}>
@@ -53,7 +111,7 @@ export default function Settings() {
         <label htmlFor="radius">km</label>{" "}
         <div>
           <p>{radius} km Radius</p>
-          <p>{interestedIn} ist augewählt</p>
+          <p>{filter}</p>
         </div>
       </div>
 
