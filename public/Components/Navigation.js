6e6db27b46aef058a136b3344f0d19247091df46
Julia Löschel <juloeschel@gmail.com>
2022-05-26 21:33:38 +0200

    delete ReactIcond for deployment

--- public/Components/Navigation.js
+++ public/Components/Navigation.js
@@ -1,43 +1,36 @@
 import styles from "../../styles/Navigation.module.css";
 import Link from "next/link";
-import { BiWorld } from "react-icons/Bi";
-import { GoLocation, GoSettings } from "react-icons/Go";
+//import { BiWorld } from "react-icons/Bi";
+//import { GoLocation, GoSettings } from "react-icons/Go";
+//<GoLocation className={styles.navIcon} />
+//  <BiWorld className={styles.navIcon} />
+//  <GoSettings className={styles.navIcon} />
 
 export default function Navigatio({ minimalLocationSet }) {
   return (
     <footer className={styles.footer}>
       <button className={styles.button}>
         <Link href="/input">
-          <a>
-            <GoLocation className={styles.navIcon} />
-          </a>
+          <a>Input</a>
         </Link>
       </button>
       {minimalLocationSet ? (
         <>
           <button className={styles.button}>
             <Link href="/">
-              <a>
-                <BiWorld className={styles.navIcon} />
-              </a>
+              <a>Map</a>
             </Link>
           </button>
           <button className={styles.button}>
             <Link href="/settings">
-              <a>
-                <GoSettings className={styles.navIcon} />
-              </a>
+              <a>Settings</a>
             </Link>
           </button>
         </>
       ) : (
         <>
-          <button className={styles.button}>
-            <BiWorld className={styles.navIconInactive} />
-          </button>
-          <button className={styles.button}>
-            <GoSettings className={styles.navIconInactive} />
-          </button>
+          <button className={styles.button}>Map</button>
+          <button className={styles.button}>Settings</button>
         </>
       )}
     </footer>
